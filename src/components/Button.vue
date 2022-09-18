20b712ace13310a0a007d79645fa600e35d45700
Samad Guliev <samadguliev@mail.ru>
2022-09-18 11:47:30 +0300

    Init

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="click()">AASSSD</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  methods: {
+    click() {
+      ethereum.request({ method: 'eth_requestAccounts' });
+    }
+  },
+  created() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
